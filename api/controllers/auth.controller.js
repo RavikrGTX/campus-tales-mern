d0d142ad2cf2a83f0c1c91a18c365dabbea3833b
ravi <ravikumarmamidi17@gmail.com>
2024-05-25 09:53:58 +0530

    practise git

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js"
+
+export const signup=async(req,res)=>{
+    const {username,email,password}=req.body;
+
+    if(!username||!email||!password||username===''||email===''||password===''){
+        return res.status(400).json({message:'all fields required'});
+
+    }
+ 
+    const newUser=new User({
+        username,
+        email,
+        password
+
+    });
+    
+    await newUser.save();
+    res.json('signup succesffull')
+}
