25309afc880dba2d36cb9a72b86105c4a39a5ac7
ravi <ravikumarmamidi17@gmail.com>
2024-05-29 15:56:42 +0530

    make signup page functioning

--- thepharmacast/src/pages/SignUp.jsx
+++ thepharmacast/src/pages/SignUp.jsx
@@ -1,8 +1,44 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Spinner } from 'flowbite-react';
+import { Link ,useNavigate } from 'react-router-dom';
+
 
 export default function SignUp() {
+ const [formData, setFormData] = useState({});
+ const [errorMessage, setErrorMessage] = useState(null);
+ const [loading, setLoading] = useState(false);
+ const navigate=useNavigate();
+ const handleChange= (e)=>{
+  setFormData({ ...formData,[e.target.id]: e.target.value.trim()});
+ };
+ const handleSubmit= async (e)=>{
+   e.preventDefault();
+   if(!formData.username||!formData.email||!formData.password){
+    return setErrorMessage('please fill out all fields.');
+   }
+   try{
+    setLoading(true);
+    setErrorMessage(null);
+    const res = await fetch('api/auth/signup',{
+      method:'POST',
+      headers: {'Content-Type':'application/json'},
+      body:JSON.stringify(formData),
+
+    });
+    const data = await res.json();
+    if(data.succes===false){
+       return setErrorMessage(data.message);
+    }
+    setLoading(false);
+    if(res.ok){
+      navigate('/sign-in');
+    }
+   }catch(error){
+     setErrorMessage(error.message);
+     setLoading(false);
+   };
+ }
   return (
     <div className='min-h-scrren mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
@@ -24,29 +60,44 @@ export default function SignUp() {
               <TextInput 
               type='text'
               placeholder='username'
-              id='username'/>
+              id='username'onChange={handleChange}/>
               </div>
             <div>
               <Label>email</Label>
               <TextInput 
-              type='text'
+              type='email'
               placeholder='Email'
-              id='email'/>
+              id='email'onChange={handleChange}/>
               </div>
             <div>
               <Label>password</Label>
               <TextInput 
-              type='text'
+              type='password'
               placeholder='password'
-              id='password'/>
+              id='password' onChange={handleChange}/>
               </div>
-              <Button gradientDuoTone='greenToBlue' type='submit'>Sign-Up</Button>
+              <Button gradientDuoTone='greenToBlue' type='submit' onClick={handleSubmit} disabled={loading}>{
+                loading?(
+                  <>
+                  <Spinner size='sm'/>
+                  <span className='pl-3'>Loading</span>
+                  </>
+                ):'Sign Up'
+              }
+              </Button>
                <div className='flex gap-3 text-sm mt-4'>
-              <span>already signedin?</span>
+              <span>already Have an account?</span>
               <Link className='text-blue-500' to="/sign-in"> 
                    sign-in 
               </Link>
               </div>
+              {
+                errorMessage&&(
+                  <Alert className='mt-5' color='failure' > 
+                    {errorMessage}
+                  </Alert>
+                )
+              }
           </form>
         </div>
       </div>
