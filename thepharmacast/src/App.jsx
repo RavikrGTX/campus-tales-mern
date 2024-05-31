b7659e1adc35954bd1c588ea2b0eac468f1ec28b
ravi <ravikumarmamidi17@gmail.com>
2024-05-31 12:42:07 +0530

    complete Footer components

--- thepharmacast/src/App.jsx
+++ thepharmacast/src/App.jsx
@@ -9,6 +9,9 @@ import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import Hero from './components/Hero/Hero'
 import Blog from './pages/Blog'
+import FooterCom from './components/FooterCom'
+
+// import FooterCom from './components/FooterCom'
 
 
 export default function App() {
@@ -25,6 +28,8 @@ export default function App() {
     <Route path="/dashboard" element={<Dashboard/>}/>
     <Route path="/contact" element={<Contact/>}/>
   </Routes>
+   <FooterCom/>
   </BrowserRouter>
+ 
   )
-} 
+}
--- thepharmacast/src/pages/SignUp.jsx
+++ thepharmacast/src/pages/SignUp.jsx
@@ -40,7 +40,7 @@ export default function SignUp() {
    };
  }
   return (
-    <div className='min-h-scrren mt-20'>
+    <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
         {/* left */}
         <div className='flex-1'> <Link
