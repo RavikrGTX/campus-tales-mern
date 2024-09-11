e012cf8ff892b2006eb5c15e3fc33fc50adde55a
ravi <ravikumarmamidi17@gmail.com>
2024-09-11 13:07:27 +0530

    add call to action to the post page

--- thepharmacast/src/components/CallToAction.jsx
+++ thepharmacast/src/components/CallToAction.jsx
@@ -5,19 +5,24 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Want to collabe with pharmacast?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Checkout instagram page of thepharmacast
             </p>
+
+
+            
+            
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://www.instagram.com/the_pharmacast/" target='_blank' rel='noopener noreferrer'>
+                    instagram
                 </a>
             </Button>
+        
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://tse1.mm.bing.net/th?id=OIP.Gk6h2B9J8tsQj_1EkGFSOQHaDd&pid=Api&P=0&h=180" />
         </div>
     </div>
   )
