f69de41a0dec96a9e76cbdf44a0ae23f7bc8212b
ravi <ravikumarmamidi17@gmail.com>
2024-09-11 23:47:10 +0530

    semi final about and projects

--- thepharmacast/src/pages/About.jsx
+++ thepharmacast/src/pages/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About ThePharmacast
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to ThePharmacast! this is a place to share my thoughts and
+              ideas with the world. Dr.Satya is a passionate pharmacist who
+              loves to write about pharma, medicine, and phd.
+            </p>
+
+            <p>
+              We’re constantly updating with fresh content, so be sure to keep
+              visiting the site for the latest insights, tips, and updates. Stay
+              tuned and don't miss out on our upcoming posts!"
+            </p>
+
+            <p></p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
