2dcdcddc2b15c154527aa2ae7bea22b31a1e3d4f
ravi <ravikumarmamidi17@gmail.com>
2025-12-17 14:32:04 +0530

    remove dashBoard componetn imoort

--- thecampustales/src/pages/Dashboard.jsx
+++ thecampustales/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
-import DashboardComp from '../components/DashBoardComp';
+// import DashboardComp from '../components/DashBoardComp';
 
 
 
