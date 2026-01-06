1d13628d974dca46e7f8a9b58c2016a131d0b060
ravi <ravikumarmamidi17@gmail.com>
2026-01-06 16:40:52 +0530

    [FIX]:Resolve Image Upload issues

--- thecampustales/src/pages/CreatePost.jsx
+++ thecampustales/src/pages/CreatePost.jsx
@@ -32,7 +32,7 @@ export default function CreatePost() {
       }
       setImageUploadError(null);
         const storage = getStorage(app);
-        const fileName=new Date().getTime()+ '-'+ file.name;
+        const fileName=new Date().getTime()+'-'+ file.name;
         const storageRef=ref(storage,fileName);
         const uploadTask=uploadBytesResumable(storageRef,file);
 
